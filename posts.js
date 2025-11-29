8dd347a2a024e30803b6a069ca537cc32d73f316
Janiru Hansaga Perera <janiruhansaga2025@gmail.com>
2025-11-29 14:05:24 +0530

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,12 @@
+// posts.js
+// This is your "Database". Add new posts to the TOP of this list.
+
+const blogPosts = [
+    {
+        title: "Welcome to My Blog",
+        date: "2025-11-29",
+        image: "hero.png", // Use an image file path (like 'images/pic.jpg')
+        content: "This is my first post! Since I am hosting on GitHub Pages, I am using a static JavaScript file to store my posts. This ensures everyone can see them!"
+    }
+    // You will paste new posts here...
+];
